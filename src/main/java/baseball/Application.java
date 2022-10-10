1708ec8da5b80347b020cdc2a84fda9fba5ea2b9
swonny <wonny921@gmail.com>
2022-10-10 15:55:42 +0900

    feat: '게임 시작'기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,15 @@ import  camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-        String tmp = (String)Console.readLine();
-        System.out.println(tmp);
+        String repeat;
+        do {
+            startGame();
+            repeat = Console.readLine();
+            System.out.println(repeat);
+        } while (repeat.equals("1"));
+    }
+
+    public static void startGame() {
+        System.out.println("Game has started!");
     }
 }
