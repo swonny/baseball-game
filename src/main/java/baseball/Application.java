a069a7feca37ba7bae531b6759fa0aa05ad46781
swonny <wonny921@gmail.com>
2022-10-10 20:29:48 +0900

    feat: 정답 비교 기능 구현
    Answers 클래스에 정답 비교를 위한 getHint 메서드 추가:
    - 기능 분리를 위해 getHint 메서드를 구현하였다.
    - 정답(answers)과 사용자 입력(userAnswers)을 비교한다.
    - 힌트를 출력한다.
    - 정답이 틀렸으면 false를, 맞았으면 true를 반환한다.
    
    countBall 메서드와 countStrike 메서드 구현:
    - 기능 분리를 위해 두 메서드를 따로 구현하였다.
    - 각각 ball과 strike 개수를 반환한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,6 @@ package baseball;
 import  camp.nextstep.edu.missionutils.Randoms;
 import  camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
@@ -14,42 +11,83 @@ public class Application {
         do {
             Game game = new Game();
             game.startGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             repeat = Console.readLine();
         } while (repeat.equals("1"));
     }
 }
 
 class Game {
-//    private List<Integer> answers = new ArrayList<Integer>(3); // String으로 변경하기
     String answers = new String();
 
     public void startGame() {
         Answer answer = new Answer();
         answers = answer.createRandomNumbers();
-        boolean repeat = true;
+        System.out.println(answers);
+        boolean correct = true;
 
         do {
             String userAnswers = new String();
             System.out.print("숫자를 입력해주세요 : ");
-            userAnswers = Console.readLine();
-            System.out.println(answers);
+            userAnswers = Console.readLine(); // 숫자 입력
+            correct = answer.getHint(answers, userAnswers);
 
-            if (answers.equals(userAnswers)) {
-                repeat = false;
-            }
-
-        } while (repeat);
+        } while (!correct);
     }
 }
 
 class Answer {
     public String createRandomNumbers() {
         String answers = new String();
-
         for (int i = 0; i < 3; i++) {
             answers += Integer.toString(Randoms.pickNumberInRange(1, 9));
         }
 
         return answers;
+    }
+
+    public boolean getHint(String answers, String userAnswers) {
+        int strike = countStrike(answers, userAnswers);
+        int ball = countBall(answers, userAnswers);
+        ball -= strike;
+
+        String output = new String();
+        if (ball > 0) {
+            output += Integer.toString(ball) + "볼 ";
+        }
+        if (strike > 0) {
+            output += Integer.toString(strike) + "스트라이크 ";
+        }
+        if (ball == 0 && strike == 0) {
+            output += "낫싱";
+        }
+        System.out.println(output);
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
+
+    private int countBall(String answers, String userAnswers) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (userAnswers.indexOf(answers.charAt(i)) > -1) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    private int countStrike(String answers, String userAnswers) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answers.charAt(i) == userAnswers.charAt(i)) {
+                count += 1;
+            }
+        }
+
+        return count;
     }
 }
