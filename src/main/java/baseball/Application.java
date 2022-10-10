038fe5f1b58ff9552abde725d00af17d39ece30c
swonny <wonny921@gmail.com>
2022-10-10 18:14:18 +0900

    refactor: 1번 기능 수정
    Game 클래스 분리
    - Application 내부에 있던 startGame 함수를 수정하였다.
    - Game 실행과 관련된 내용은 Game 클래스로 분리하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,18 +2,23 @@ package baseball;
 
 import  camp.nextstep.edu.missionutils.Randoms;
 import  camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-        String repeat;
+        String repeat = new String();
         do {
-            startGame();
+            Game game = new Game();
+            game.startGame();
             repeat = Console.readLine();
-            System.out.println(repeat);
         } while (repeat.equals("1"));
     }
+}
 
-    public static void startGame() {
+class Game {
+    public void startGame() {
         System.out.println("Game has started!");
     }
 }
+
